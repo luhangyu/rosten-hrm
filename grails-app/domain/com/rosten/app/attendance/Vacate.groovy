78a4eb9abea34c773ba02f1b05aacddb87ab05c3
chinarosten <luhangyu2000@163.com>
2014-07-27 15:28:06 +0800

    完善流程字段

--- grails-app/domain/com/rosten/app/attendance/Vacate.groovy
+++ grails-app/domain/com/rosten/app/attendance/Vacate.groovy
@@ -1,8 +1,13 @@
 package com.rosten.app.attendance
 
 import java.util.Date;
+
 import com.rosten.app.system.User
+import com.rosten.app.annotation.GridColumn
+import com.rosten.app.system.Company
+
 import java.text.SimpleDateFormat
+
 /**
  * 请假申请
  * @author xucy
@@ -12,9 +17,19 @@ class Vacate {
 
 	String id
 	
-	//开始时间
-	Date startDate
+	@GridColumn(name="拟稿人",width="50px",colIdx=1)
+	def getFormattedDrafter(){
+		if(user!=null){
+			return user.getFormattedName()
+		}else{
+			return ""
+		}
+	}
 	
+	//开始时间
+	Date startDate = new Date()
+	
+	@GridColumn(name="开始时间",width="106px",colIdx=2)
 	def getFormatteStartDate(){
 		if(startDate!=null){
 			SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd")
@@ -25,8 +40,9 @@ class Vacate {
 	}
 	
 	//结束时间
-	Date endDate
+	Date endDate = new Date()
 	
+	@GridColumn(name="结束时间",width="106px",colIdx=3)
 	def getFormatteEndDate(){
 		if(endDate!=null){
 			SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd")
@@ -42,18 +58,99 @@ class Vacate {
 	String unitType = "天"//小时或者天
 	
 	//请假类型
+	@GridColumn(name="类型",width="60px",colIdx=4)
 	String vacateType = "事假"
 	
+	//请假内容
 	String remark
+	
+	@GridColumn(name="状态",width="60px",colIdx=6)
+	String status = "新建"
+	
+	//创建时间
+	Date createDate = new Date()
+
+	@GridColumn(name="创建时间",width="106px",colIdx=7)
+	def getFormattedCreatedDate(){
+		if(createDate!=null){
+			SimpleDateFormat sd = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+			return sd.format(createDate)
+		}else{
+			return ""
+		}
+	}
+	
+	//前处理人
+	User frontUser
+	
+	//前处理部门
+	String frontDepart
+	
+	//前处理时间
+	Date frontDealDate
+	
+	//当前处理人
+	User currentUser
+
+	@GridColumn(name="当前处理人",width="60px",colIdx=5)
+	def getCurrentUserName(){
+		if(currentUser!=null){
+			return currentUser.getFormattedName()
+		}else{
+			return ""
+		}
+	}
+
+	//当前处理部门
+	//@GridColumn(name="处理部门",width="60px",colIdx=7)
+	String currentDepart
+
+	//当前处理时间
+	Date currentDealDate
+	
+	//已阅读人员,读者
+	static hasMany=[hasReaders:User,readers:User]
+	
+	//缺省读者；*:允许所有人查看,[角色名称]:允许角色,user:普通人员查看
+	String defaultReaders="[普通人员],[请假管理员]"
+	def addDefaultReader(String userRole){
+		if(defaultReaders==null || "".equals(defaultReaders)){
+			defaultReaders = userRole
+		}else{
+			defaultReaders += "," + userRole
+		}
+	}
+	
+	//-------------------------流程引擎----------
+	//流程定义id
+	String processDefinitionId
+	
+	//流程id
+	String processInstanceId
+	
+	//任务id
+	String taskId
+	
+	//-------------------------流程引擎----------
 	
     static constraints = {
 		remark nullable:true,blank:true
+		number nullable:true,blank:true
+		
+		frontUser nullable:true,blank:true
+		frontDepart nullable:true,blank:true
+		frontDealDate nullable:true,blank:true
+		
+		processDefinitionId nullable:true,blank:true
+		processInstanceId nullable:true,blank:true
+		taskId nullable:true,blank:true
     }
-	static belongsTo = [user:User]
+	
+	static belongsTo = [user:User,company:Company]
 	
 	static mapping = {
 		id generator:'uuid.hex',params:[separator:'-']
-		table "ROSTEN_Attendance_vacate"
+		table "ROSTEN_ATTENDANCE_VACATE"
 		remark sqlType:"text"
 	}
 	
