7233d07483ac7c6f82f152ffb68665c76afdb514
xucy <xucy@xucy-PC>
2014-08-10 16:46:02 +0800

    公共

---
+++ grails-app/services/com/rosten/app/share/ShareService.groovy
@@ -0,0 +1,17 @@
+package com.rosten.app.share
+
+class ShareService {
+
+  def getAllDepartByChild ={departList,depart->
+		departList << depart
+		if(depart.children){
+			depart.children.each{
+				return getAllDepartByChild(departList,it)
+			}
+		}else{
+			return departList
+		}
+	}
+  
+  
+}
