266a8cc23ac3904951653fe5930a900747737959
chinarosten <luhangyu2000@163.com>
2014-09-30 14:39:54 +0800

    完善弹出对话框中title自定义功能

--- web-app/js/rosten/widget/ShowDialog.js
+++ web-app/js/rosten/widget/ShowDialog.js
@@ -1,6 +1,6 @@
 define(["dojo/_base/declare","dojo/_base/connect","dijit/_WidgetBase","dijit/_TemplatedMixin","rosten/widget/RostenDialog"],function(_1,_2,_3,_4,_5){
-return _1("rosten.widget.ShowDialog",[_3,_4],{templateString:"<div dojoAttachPoint=\"containerNode\"></div>",dialog:null,src:"",onLoadFunction:null,postCreate:function(){
-this.dialog=new _5({id:"rosten_showDialog",renderStyles:true},this.containerNode.domNode);
+return _1("rosten.widget.ShowDialog",[_3,_4],{templateString:"<div dojoAttachPoint=\"containerNode\"></div>",dialog:null,src:"",onLoadFunction:null,title:"系统对话框",postCreate:function(){
+this.dialog=new _5({id:"rosten_showDialog",title:this.title,renderStyles:true},this.containerNode.domNode);
 if(this.src==""){
 this.dialog.setContent("error:can not get the data......");
 }else{
--- web-app/js/rosten/widget/ShowDialog1.js
+++ web-app/js/rosten/widget/ShowDialog1.js
@@ -1,6 +1,6 @@
 define(["dojo/_base/declare","dojo/_base/connect","dijit/_WidgetBase","dijit/_TemplatedMixin","dojox/layout/ContentPane","rosten/widget/RostenDialog"],function(_1,_2,_3,_4,_5,_6){
-return _1("rosten.widget.ShowDialog1",[_3,_4],{templateString:"<div dojoAttachPoint=\"containerNode\"></div>",dialog:null,dlgContentPane:null,src:"",position_absolute_x:200,position_absolute_y:200,callback:null,callbackargs:null,onLoadFunction:null,postCreate:function(){
-this.dialog=new _6({id:"rosten_showDialog",renderStyles:true},this.containerNode.domNode);
+return _1("rosten.widget.ShowDialog1",[_3,_4],{templateString:"<div dojoAttachPoint=\"containerNode\"></div>",dialog:null,dlgContentPane:null,src:"",position_absolute_x:200,position_absolute_y:200,callback:null,callbackargs:null,onLoadFunction:null,title:"系统对话框",postCreate:function(){
+this.dialog=new _6({id:"rosten_showDialog",title:this.title,renderStyles:true},this.containerNode.domNode);
 if(this.src==""){
 this.dialog.setContent("error:can not get the data......");
 }else{
