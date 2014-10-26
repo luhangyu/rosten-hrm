4f217a1e6c751f82f45992f6cdd0386e64404c4b
xucy <xucy@xucy-PC>
2014-10-26 16:06:06 +0800

    合同

---
+++ grails-app/controllers/com/rosten/app/bargain/BargainController.groovy
@@ -0,0 +1,15 @@
+package com.rosten.app.bargain
+
+class BargainController {
+	def springSecurityService
+	
+   	def getBargainPerson={
+		def model =[:]
+		def currentUser = springSecurityService.getCurrentUser()
+		def company = currentUser.company
+		model["company"] = company
+		
+		render(view:'/staff/bargainPerson',model:model)
+		
+	}
+}
