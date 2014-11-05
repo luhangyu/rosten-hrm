55ad631ac225810e72b0457d5620b73f28145855
xucy <xucy@xucy-PC>
2014-11-05 22:22:55 +0800

    导入

--- src/java/com/rosten/app/export/ExcelImport.java
+++ src/java/com/rosten/app/export/ExcelImport.java
@@ -5,6 +5,7 @@ import java.io.File;
 import jxl.Sheet;
 import jxl.Workbook;
 
+import com.rosten.app.staff.ContactInfor;
 import com.rosten.app.staff.PersonInfor;
 import com.rosten.app.staff.StaffService;
 import com.rosten.app.system.User;
@@ -20,23 +21,45 @@ public class ExcelImport {
 		StaffService _service = new StaffService();
 
 		for(int i=1;i<sourceRowCount;i++){
-			 String ssbm =sourceSheet.getCell(0, i).getContents();	//所属部门
-			 String xm =sourceSheet.getCell(1, i).getContents();	//姓名
-			 String xb =sourceSheet.getCell(2, i).getContents();	//性别
-			 String csny =sourceSheet.getCell(3, i).getContents();	//出生年月
-			 String xl =sourceSheet.getCell(4, i).getContents();	//学历
-			 String zzmm =sourceSheet.getCell(5, i).getContents();	//政治面貌
-			 String jg =sourceSheet.getCell(6, i).getContents();	//籍贯
+			 String ssbm =sourceSheet.getCell(1, i).getContents();	//所属部门
+			 String xm =sourceSheet.getCell(2, i).getContents();	//姓名
+			 String xb =sourceSheet.getCell(3, i).getContents();	//性别
+			 String hyzk =sourceSheet.getCell(4, i).getContents();	//婚姻状况
+			 String jg =sourceSheet.getCell(5, i).getContents();	//籍贯
+			 String sfzh =sourceSheet.getCell(6, i).getContents();	//身份证号
+			 String byxx =sourceSheet.getCell(7, i).getContents();	//毕业学校
+			 
+			 String sxzy =sourceSheet.getCell(8, i).getContents();	//所学专业
+			 String xl =sourceSheet.getCell(9, i).getContents();	//学历
+			 String zc =sourceSheet.getCell(10, i).getContents();	//职称
+			 String zzmm =sourceSheet.getCell(11, i).getContents();	//政治面貌
+			 String gzgw =sourceSheet.getCell(12, i).getContents();	//工作岗位
+			 String ygxs =sourceSheet.getCell(13, i).getContents();	//用功形式
+			 String cjgzsj =sourceSheet.getCell(14, i).getContents();	//参加工作时间
+			 String dxhgzsj =sourceSheet.getCell(15, i).getContents();	//到协会工作时间
+			 String sjhm =sourceSheet.getCell(16, i).getContents();	//手机号码
+			 
+			 
+			 ContactInfor con = new ContactInfor();
+			 con.setMobile(null==sjhm?"":sjhm);
 			 
 			//根据用户名插入对应数据
 			PersonInfor personInfor = new PersonInfor();
 			personInfor.setChinaName(xm);
-			personInfor.setUser(userEntity);
+			//personInfor.setUser(userEntity);
 			personInfor.setUserTypeEntity(userType);
 			personInfor.setSex(xb);
-			//personInfor.setBirthday(csny);
+			personInfor.setMarriage(hyzk);
 			personInfor.setNativeAddress(jg);
-			personInfor.setIdCard("000001");
+			personInfor.setIdCard(sfzh);
+			personInfor.setSchoolName(byxx);
+			
+			personInfor.setUpDegree(xl);
+			personInfor.setTechGrade(zc);
+			personInfor.setPoliticsStatus(zzmm);
+			personInfor.setWorkJob(gzgw);
+		//	personInfor.setWorkJobDate(cjgzsj);
+			
 			
 			_service.commonSave(personInfor,ssbm,userEntity);
 			
